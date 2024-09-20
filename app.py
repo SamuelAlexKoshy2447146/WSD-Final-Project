0609601c28616bcab6a417a647d22905b4dbbad4
Sam Alex Koshy <samuel.koshy@mca.christuniversity.in>
2024-09-20 22:36:04 +0530

    refactor static and templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, send_from_directory
+from flask import Flask, request, jsonify, send_from_directory, render_template
 from flask_cors import CORS
 from flask_socketio import SocketIO, emit
 import subprocess
@@ -7,31 +7,36 @@ import os
 
 app = Flask(__name__)
 CORS(app)
-socketio = SocketIO(app, cors_allowed_origins='*')
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-@app.route('/')
-def index():
-    return send_from_directory('', 'home.html') #initial page name
 
-@app.route('/editor') 
+@app.route("/")
+def home():
+    return render_template("home.html")  # initial page name
+
+
+@app.route("/editor")
 def editor():
-    return send_from_directory('', 'Compiler.html')
+    return send_from_directory("", "Compiler.html")
 
-@app.route('/about') 
-def editor():
-    return send_from_directory('', 'about.html')
 
-@app.route('/run', methods=['POST'])
+@app.route("/about")
+def about():
+    return send_from_directory("", "about.html")
+
+
+@app.route("/run", methods=["POST"])
 def run_code():
     data = request.json
-    code = data.get('code', '')
-    inputs = data.get('inputs', [])
+    if data is not None:
+        code = data.get("code", "")
+        inputs = data.get("inputs", [])
 
     if not code:
-        return jsonify({'output': 'No code provided'}), 400
+        return jsonify({"output": "No code provided"}), 400
 
-    filename = 'temp_code.py'
-    with open(filename, 'w') as file:
+    filename = "temp_code.py"
+    with open(filename, "w") as file:
         file.write(code)
 
     try:
@@ -40,23 +45,25 @@ def run_code():
             stdin=subprocess.PIPE,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
-            text=True
+            text=True,
         )
 
-        stdout, stderr = process.communicate(input='\n'.join(inputs), timeout=10)
+        stdout, stderr = process.communicate(input="\n".join(inputs), timeout=10)
         output = stdout + stderr
     except subprocess.TimeoutExpired:
         process.kill()
-        output = 'Error: Code execution timeout'
+        output = "Error: Code execution timeout"
     finally:
         os.remove(filename)
 
-    return jsonify({'output': output})
+    return jsonify({"output": output})
 
-@socketio.on('code_change')
+
+@socketio.on("code_change")
 def handle_code_change(data):
-    print('Code change received:', data) 
-    emit('code_update', data, broadcast=True)
+    print("Code change received:", data)
+    emit("code_update", data, broadcast=True)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     socketio.run(app, debug=True)
